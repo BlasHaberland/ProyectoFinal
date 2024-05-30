85f980fc773bd842ef51fb6d06f30ef7d35e7958
Blas Haberland <haberlandblas@gmail.com>
2024-05-30 20:36:30 -0300

    Add Model ColectivoData

--- src/main/java/com/mycompany/proyectofinal/ProyectoFinal.java
+++ src/main/java/com/mycompany/proyectofinal/ProyectoFinal.java
@@ -1,8 +1,10 @@
 package com.mycompany.proyectofinal;
 
 import DAO.CiudadData;
+import DAO.ColectivoData;
 import DAO.PasajeroData;
 import Modelos.Ciudad;
+import Modelos.Colectivo;
 import Modelos.Pasajero;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,65 +12,97 @@ import java.util.List;
 public class ProyectoFinal {
 
     public static void main(String[] args) {
-        //PasajeroData
-        //        //borrarPasajero
-        //        PasajeroData pasajeroData = new PasajeroData();
-        //        System.out.println(pasajeroData.borrarPasajero(1));
-        //
-        //        //modificarPasajero
-        //        PasajeroData pasajeroData = new PasajeroData();
-        //        Pasajero pasajero = pasajeroData.obtenerPasajeroPorId(1);
-        //        pasajero.setEstado(true);
-        //        System.out.println(pasajeroData.modificarPasajero(pasajero));
-        //
-        //        //crearPasajero
-        //        PasajeroData pasajeroData = new PasajeroData();
-        //        Pasajero pasajero = new Pasajero("Brad", "Pitt", 32876543, "elpitt@gmail.com", "15432876345", true);
-        //        pasajeroData.crearPasajero(pasajero);
-        //
-        //        //obtenerPasajeroPorDni
-        //        PasajeroData pasajeroData = new PasajeroData();
-        //        System.out.println(pasajeroData.obtenerPasajeroPorDni(42133651));
-        //
-        //        //obtenerPasajeroPorId
-        //        PasajeroData pasajeroData = new PasajeroData();
-        //        System.out.println(pasajeroData.obtenerPasajeroPorId(1));
-        //
-        //        //listarPasajeros
-        //        List<Pasajero> pasajeros = new ArrayList<>();
-        //        PasajeroData pasajeroData = new PasajeroData();
-        //        pasajeros = pasajeroData.obtenerPasajeros();
-        //        System.out.println(pasajeros);
-        //
-        //CiudadData
-        //        //borrarCiudad
-        //        CiudadData ciudadData = new CiudadData();
-        //        System.out.println(ciudadData.borrarCiudad(10));
-        //
-        //        //modificarCiudad
-        //        CiudadData ciudadData = new CiudadData();
-        //        Ciudad ciudad = ciudadData.obtenerCiudadPorId(1);
-        //        ciudad.setEstado(false);
-        //        System.out.println(ciudadData.modificarCiudad(ciudad));
-        //
-        //        //crearCiudad
-        //        CiudadData ciudadData = new CiudadData();
-        //        Ciudad ciudad = new Ciudad(" San Salvador de Jujuy", "Jujuy", true);
-        //        System.out.println(ciudadData.crearCiudad(ciudad));
-        //
-        //        //buscarCiudadPorNombre
-        //        CiudadData ciudadData = new CiudadData();
-        //        String ciudad = "Merlo";
-        //        System.out.println(ciudadData.obtenerCiudadPorNombre(ciudad));
-        //
-        //        //obtenerCiudadPorId
-        //        CiudadData ciudadData = new CiudadData();
-        //        System.out.println(ciudadData.obtenerCiudadPorId(8));
-        //
-        //        //listarCiudades
-        //        List<Ciudad> ciudades = new ArrayList<>();
-        //        CiudadData ciudadData = new CiudadData();
-        //        ciudades = ciudadData.obtenerCiudades();
-        //        System.out.println(ciudades);
+//      ColectivoData
+//        //borrarColectivo
+//        ColectivoData colectivoData = new ColectivoData();
+//        System.out.println(colectivoData.borrarColectivo(1));
+//
+//        //modificarColectivo
+//        ColectivoData colectivoData = new ColectivoData();
+//        Colectivo colectivo = colectivoData.obtenerColectivoPorId(1);
+//        colectivo.setEstado(true);
+//        System.out.println(colectivoData.modificarColectivo(colectivo));
+//
+//        //crearColectivo
+//        ColectivoData colectivoData = new ColectivoData();
+//        Colectivo colectivo = new Colectivo("AB123CD", "Volvo", "Travel", 50, true);
+//        System.out.println(colectivoData.crearColectivo(colectivo));
+//
+//        //obternerColectivoPorMatricula
+//        ColectivoData colectivoData = new ColectivoData();
+//        Colectivo colectivo = colectivoData.obtenerColectivoPorId(1);
+//        System.out.println(colectivoData.obtenerColectivoPorMatricula(colectivo.getMatricula()));
+//
+//        //obtenerColectivoPorId
+//        ColectivoData colectivoData = new ColectivoData();
+//        Colectivo colectivo = colectivoData.obtenerColectivoPorId(1);
+//        System.out.println(colectivoData.obtenerColectivoPorId(colectivo.getIdColectivo()));
+//
+//        //obtenerColectivos
+//        ColectivoData colectivoData = new ColectivoData();
+//        List<Colectivo> colectivos = new ArrayList<>();
+//        colectivos = colectivoData.obtenerColectivos();
+//        System.out.println(colectivos);
+//
+//      PasajeroData
+//        //borrarPasajero
+//        PasajeroData pasajeroData = new PasajeroData();
+//        System.out.println(pasajeroData.borrarPasajero(1));
+//
+//        //modificarPasajero
+//        PasajeroData pasajeroData = new PasajeroData();
+//        Pasajero pasajero = pasajeroData.obtenerPasajeroPorId(1);
+//        pasajero.setEstado(true);
+//        System.out.println(pasajeroData.modificarPasajero(pasajero));
+//
+//        //crearPasajero
+//        PasajeroData pasajeroData = new PasajeroData();
+//        Pasajero pasajero = new Pasajero("Brad", "Pitt", 32876543, "elpitt@gmail.com", "15432876345", true);
+//        pasajeroData.crearPasajero(pasajero);
+//
+//        //obtenerPasajeroPorDni
+//        PasajeroData pasajeroData = new PasajeroData();
+//        System.out.println(pasajeroData.obtenerPasajeroPorDni(42133651));
+//
+//        //obtenerPasajeroPorId
+//        PasajeroData pasajeroData = new PasajeroData();
+//        System.out.println(pasajeroData.obtenerPasajeroPorId(1));
+//
+//        //listarPasajeros
+//        List<Pasajero> pasajeros = new ArrayList<>();
+//        PasajeroData pasajeroData = new PasajeroData();
+//        pasajeros = pasajeroData.obtenerPasajeros();
+//        System.out.println(pasajeros);
+
+//      CiudadData
+//        //borrarCiudad
+//        CiudadData ciudadData = new CiudadData();
+//        System.out.println(ciudadData.borrarCiudad(10));
+//
+//        //modificarCiudad
+//        CiudadData ciudadData = new CiudadData();
+//        Ciudad ciudad = ciudadData.obtenerCiudadPorId(1);
+//        ciudad.setEstado(false);
+//        System.out.println(ciudadData.modificarCiudad(ciudad));
+//
+//        //crearCiudad
+//        CiudadData ciudadData = new CiudadData();
+//        Ciudad ciudad = new Ciudad(" San Salvador de Jujuy", "Jujuy", true);
+//        System.out.println(ciudadData.crearCiudad(ciudad));
+//
+//        //buscarCiudadPorNombre
+//        CiudadData ciudadData = new CiudadData();
+//        String ciudad = "Merlo";
+//        System.out.println(ciudadData.obtenerCiudadPorNombre(ciudad));
+//
+//        //obtenerCiudadPorId
+//        CiudadData ciudadData = new CiudadData();
+//        System.out.println(ciudadData.obtenerCiudadPorId(8));
+//
+//        //listarCiudades
+//        List<Ciudad> ciudades = new ArrayList<>();
+//        CiudadData ciudadData = new CiudadData();
+//        ciudades = ciudadData.obtenerCiudades();
+//        System.out.println(ciudades);
     }
 }
