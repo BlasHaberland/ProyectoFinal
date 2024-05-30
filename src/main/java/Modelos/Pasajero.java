86a601449512f0a62aaa111708be0c1fdaa630d1
Federico González <alambratore@hotmail.com>
2024-05-30 09:22:11 -0300

    add Pasajero Model

---
+++ src/main/java/Modelos/Pasajero.java
@@ -0,0 +1,112 @@
+package Modelos;
+
+import java.util.Objects;
+
+public class Pasajero {
+
+  private int idPasajero;
+  private String nombre;
+  private String apellido;
+  private String dni;
+  private String correo;
+  private String telefono;
+
+  public Pasajero() {
+  }
+
+  public Pasajero(String nombre, String apellido, String dni, String correo, String telefono) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.dni = dni;
+    this.correo = correo;
+    this.telefono = telefono;
+  }
+
+  public Pasajero(int idPasajero, String nombre, String apellido, String dni, String correo, String telefono) {
+    this.idPasajero = idPasajero;
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.dni = dni;
+    this.correo = correo;
+    this.telefono = telefono;
+  }
+
+  public int getIdPasajero() {
+    return idPasajero;
+  }
+
+  public void setIdPasajero(int idPasajero) {
+    this.idPasajero = idPasajero;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public String getApellido() {
+    return apellido;
+  }
+
+  public void setApellido(String apellido) {
+    this.apellido = apellido;
+  }
+
+  public String getDni() {
+    return dni;
+  }
+
+  public void setDni(String dni) {
+    this.dni = dni;
+  }
+
+  public String getCorreo() {
+    return correo;
+  }
+
+  public void setCorreo(String correo) {
+    this.correo = correo;
+  }
+
+  public String getTelefono() {
+    return telefono;
+  }
+
+  public void setTelefono(String telefono) {
+    this.telefono = telefono;
+  }
+
+  @Override
+  public int hashCode() {
+    int hash = 7;
+    hash = 53 * hash + Objects.hashCode(this.dni);
+    hash = 53 * hash + Objects.hashCode(this.correo);
+    return hash;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    final Pasajero other = (Pasajero) obj;
+    if (!Objects.equals(this.dni, other.dni)) {
+      return false;
+    }
+    return Objects.equals(this.correo, other.correo);
+  }
+
+  @Override
+  public String toString() {
+    return dni + " - " + apellido + ", " + nombre;
+  }
+}
