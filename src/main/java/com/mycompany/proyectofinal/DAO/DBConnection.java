6d28143b316fd6adc8c4573fe8edc677da8de3d2
Federico González <alambratore@hotmail.com>
2024-05-30 09:08:59 -0300

    First commit

---
+++ src/main/java/com/mycompany/proyectofinal/DAO/DBConnection.java
@@ -0,0 +1,37 @@
+package com.mycompany.proyectofinal.DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+  private final Connection connection;
+  private static DBConnection dbCon;
+
+  private DBConnection() throws SQLException {
+    try {
+      Class.forName("com.mysql.cj.jdbc.Driver");
+    } catch (ClassNotFoundException ex) {
+      System.err.println("Error en el Driver de MySQL");
+      System.err.println(ex);
+    }
+    connection = DriverManager.getConnection("jdbc:mysql://root:@localhost:3306/proyecto_final"); // Cadena de conexión a la base de datos
+  }
+
+  public static DBConnection getInstance() {
+    if (dbCon == null) {
+      try {
+        dbCon = new DBConnection();
+      } catch (SQLException ex) {
+        System.err.println("Error en MySQL");
+        System.err.println(ex);
+      }
+    }
+    return dbCon;
+  }
+
+  public Connection getConnection() {
+    return connection;
+  }
+}
